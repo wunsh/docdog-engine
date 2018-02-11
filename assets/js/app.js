d581c45b64c325202aa9abea5c66327ae937c724
Jaroslav <jarosluv@wunsh.ru>
2018-02-11 19:54:37 +0300

    Add Metrika goals

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,19 @@ const elmDiv = document.querySelector("#elmApp");
 if (elmDiv) {
     Elm.Main.embed(elmDiv);
 }
+
+let el = document.getElementById("signInGithub")
+if (el) {
+	el.addEventListener("click", () => { yaCounter47642428.reachGoal("SIGN_IN-GITHUB"); }, false)
+}
+
+el = document.getElementById("projectForm")
+if (el) {
+	el.addEventListener("submit", () => { yaCounter47642428.reachGoal("ADD_NEW-PROJECT"); }, false)
+}
+
+el = document.getElementById("documentForm")
+if (el) {
+	el.addEventListener("submit", () => { yaCounter47642428.reachGoal("ADD_NEW-DOCUMENT"); }, false)	
+}
+
