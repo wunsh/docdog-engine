98054675fd60f6104191b90dcdd0eb64c7828316
Jaroslav <jarosluv@wunsh.ru>
2018-02-17 02:32:26 +0300

    Fix secret key base for Travis

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,7 @@ use Mix.Config
 # you can enable the server option below.
 config :docdog, DocdogWeb.Endpoint,
   http: [port: 4001],
+  secret_key_base: "+xONLYFORTESTSynmgIFCtSnwLoh7SP1PTPzJJq0Yh4vVDiY3O4+kbhlm+svVwS1",
   server: false
 
 # Print only warnings and errors during test
